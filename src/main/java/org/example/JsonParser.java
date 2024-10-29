8e20bc0a35c630c9791bf87444acd5e58b1a695e
Liis Usin, Mikk Sarv <liis.usin@gmail.com, mikksrv@gmail.com>
2024-10-29 16:44:11 +0200

    [add] Null, integer, double test working

--- src/main/java/org/example/JsonParser.java
+++ src/main/java/org/example/JsonParser.java
@@ -1,23 +1,100 @@
 package org.example;
 
+import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 
 /// * language=json */
 public class JsonParser {
+
     public Object parse(String input) {
-        if (input.equals("null")) return null;
-        if (input.matches("-?[0-9]+")) return Integer.valueOf(input);
-        if (input.matches("[-+]?\\d*\\.?\\d+([eE][-+]?\\d+)?")) return Double.valueOf(input);
-        if (input.startsWith("\"") && input.endsWith("\""))  return input.substring(1, input.length() - 1);
-        return parse(new StringReader(input));
+        try {
+            return parse(new StringReader(input));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public Object parse(Reader input) {
-        System.out.println(input);
+    public Object parse(Reader input) throws IOException {
+        int theCharnum = input.read();
 
-        return input;
+        while (theCharnum != -1) {
+            char c = (char) theCharnum;
+            if (c == 'n') return checkNull(input);
+            if (Character.isDigit(c)) return checkNumber(input, c);
+
+//            if (c == '"') return checkNumber(input);
+//            if (c == '{') return checkObject(input);
+//            if (c == '[') return checkArray(input);
+//            if (c == 't' || c == 'f') return checkBool(input);
+            theCharnum = input.read();
+
+        }
+
+        return new IllegalArgumentException("Oh nooo!");
+    }
+
+    private Object checkNull(Reader value) throws IOException {
+        if (value.read() == 'u' && value.read() == 'l' && value.read() == 'l') return null;
+        return new IllegalArgumentException("Oh nooo! No Null");
+    }
+
+
+    private Object checkNumber(Reader value, char first) throws IOException {
+
+        StringBuilder line = new StringBuilder();
+        line.append(first);
+        int theCharnum = value.read();
+
+        while (theCharnum != -1) {
+            char c = (char) theCharnum;
+
+            if (c == '.') line.append(c);
+            else if (!Character.isDigit(c)) break;
+            if (Character.isDigit(c)) line.append(c);
+            theCharnum = value.read();
+        }
+
+        int dots = (int) line.toString().chars().filter(c -> c == '.').count();
+        if (!line.toString().contains(".")) {
+            return Integer.valueOf(line.toString());
+        } else if (dots > 1) {
+            return line.toString();
+        } else if (dots == 1) {
+            return Double.valueOf(line.toString());
+        } else {
+            return new IllegalArgumentException("Oh nooo! Not a number");
+        }
+    }
+
+    private Object checkString(Reader input) {
+        return "";
     }
 
 
 }
+
+
+//        if (input.equals("null")) return null;
+//        if (input.matches("-?[0-9]+")) return Integer.valueOf(input);
+//        if (input.matches("[-+]?\\d*\\.?\\d+([eE][-+]?\\d+)?")) return Double.valueOf(input);
+//        if (input.startsWith("\"") && input.endsWith("\"")) return input.substring(1, input.length() - 1);
+//        if (input.toCharArray()[0] == '[' && input.toCharArray()[input.length() - 1] == ']') return new ArrayList<>();
+//        if (input.equals("true") || input.equals("false")) return Boolean.parseBoolean(input);
+
+
+//        StringBuilder line = new StringBuilder();
+//        int theCharnum = value.read();
+//
+//        char[] nulls = new char[]{'u', 'l', 'l'};
+//        int i = 0;
+//
+//        while (value.read() != -1) {
+//            char c = (char) theCharnum;
+//            if (c != nulls[i++]) return "";
+//
+//            line.append(c);
+//            if (i == 3) break;
+//            theCharnum = value.read();
+//        }
+//        return line.toString().equals("ull") ? null : new IllegalArgumentException("Oh nooo! No Null");
--- src/test/java/org/example/JsonParserTest.java
+++ src/test/java/org/example/JsonParserTest.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import static java.util.Collections.emptyList;
+import static java.util.Collections.emptyMap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
@@ -10,6 +12,7 @@ class JsonParserTest {
 
     @Test
     void jsonNull() {
+        //todo does not work with anull or nulla
         assertNull(parser.parse("null"));
     }
 
@@ -28,10 +31,19 @@ class JsonParserTest {
         assertEquals("hello", parser.parse("\"hello\""));
     }
 
+    @Test
+    void emptyArray() {
+        assertEquals(emptyList(), parser.parse("[]"));
+    }
 
-//    @Test
-//    void emptyArray() {
-//        assertEquals(emptyList(), parser.parse("[]"));
-//    }
+    @Test
+    void bool() {
+        assertEquals(true, parser.parse("true"));
+        assertEquals(false, parser.parse("false"));
+    }
 
+    @Test
+    void emptyObject() {
+        assertEquals(emptyMap(), parser.parse("{}"));
+    }
 }
